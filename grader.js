d324d6b56778cb3e3bebbf051dc99ceb35c50a1e
Ubuntu <ubuntu@ip-10-254-119-9.us-west-2.compute.internal>
2013-07-16 20:22:36 +0000

    hw4 updates

--- grader.js
+++ grader.js
@@ -22,6 +22,7 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('./node_modules/restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
@@ -65,8 +66,20 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_link>', 'Url to check')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+
+    if(program.url)
+    {
+	rest.get(program.url).on('complete', function(result){
+		var checkJson = checkHtmlFile(result, program.checks);
+	});
+    }else if(program.file)
+    {
+	var checkJson = checkHtmlFile(program.file, program.checks);
+    }
+
+//    var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
